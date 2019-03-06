ee8b0a89837991bb031fdab1fa82eda4617eadd8
MaxThePrisberry <31422721+MaxThePrisberry@users.noreply.github.com>
2019-03-06 11:40:14 -0800

    Update main.js

--- main.js
+++ main.js
@@ -430,7 +430,7 @@ Boss.prototype.draw = function(bodyColor) {
 					ctx.closePath();
 					ctx.beginPath();
 					ctx.fillStyle = 'green';
-					ctx.rect(boardxtocanvasx(this.pos[0] - 144), boardytocanvasy(this.pos[1] - 264), 288 * (this.health/this.maxHealth), 12);
+					ctx.rect(boardXToCanvasX(this.pos[0] - 144), boardYToCanvasY(this.pos[1] - 264), 288 * (this.health/this.maxHealth), 12);
 					ctx.fill();
 				}
 			}
