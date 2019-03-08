56dc75b8ece13ad4ec68db483e6bb6f32f793ddf
MaxThePrisberry <31422721+MaxThePrisberry@users.noreply.github.com>
2019-03-07 16:59:01 -0800

    Update main.js

--- main.js
+++ main.js
@@ -235,6 +235,10 @@ Map.prototype.drawObjects = function() {
 }
 
 Map.prototype.drawEnemies = function() {
+	let tempTimer = new Date();
+	if ((tempTimer - startTime) % 15000 == 0){
+		this.badGuys.push(new Boss([Math.random() * this.width, Math.random() * this.height], 1, 15));
+	}
 	for (let badGuy of this.badGuys) {
 		badGuy.update();
 		if (badGuy.health <= 0){
