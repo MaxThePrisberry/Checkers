8974e5f5b5631efd4be35d30465badd9cd4048b5
Nathan Prisbrey <ntpbanana@gmail.com>
2020-04-09 01:50:22 +0000

    Made Go Server Compatible with WebSockets

--- server/listener.go
+++ server/listener.go
@@ -2,43 +2,56 @@ package main
 
 import (
 	"fmt"
-	"net"
-	"time"
-	"bufio"
+	"net/http"
+	"github.com/gorilla/websocket"
+	"log"
+//	"time"
+//	"bufio"
 )
 
-func handleConnection(conn net.Conn) {
-	if _, err := conn.Write([]byte("Hey, what's up?")); err != nil {
-		fmt.Println("Writing on the connection seems to have had an error: %v", err)
-		return
-	}
-	bufReader := bufio.NewReader(conn)
-	for {
-		conn.SetReadDeadline(time.Now().Add(10 * time.Second))
-		response, err := bufReader.ReadBytes('\n')
-		if err != nil {fmt.Println("The noob connection didn't talk a lot so we dropped it.... :/");return}
-		if _, err := conn.Write(append([]byte("I think that the following sucks: ")[:],response[:]...)); err != nil {
-			fmt.Println("Writing on the connection seems to have had an error: %v", err)
-			return
-		}
-	}
+//func handleConnection(conn net.Conn) {
+//	if _, err := conn.Write([]byte("Hey, what's up?")); err != nil {
+//		fmt.Println("Writing on the connection seems to have had an error: %v", err)
+//		return
+//	}
+//	conn.Close()
+//	bufReader := bufio.NewReader(conn)
+//	for {
+//		conn.SetReadDeadline(time.Now().Add(10 * time.Second))
+//		response, err := bufReader.ReadBytes('\n')
+//		if err != nil {fmt.Println("The noob connection didn't talk a lot so we dropped it.... :/");return}
+//		if _, err := conn.Write(append([]byte("I think that the following sucks: ")[:],response[:]...)); err != nil {
+//			fmt.Println("Writing on the connection seems to have had an error: %v", err)
+//			return
+//		}
+//	}
 //	conn.Write([]byte("You just got shut down. :)"))
 //	fmt.Println("One connection just closed itself")
 //	conn.Close()
+//}
+
+var upgrader = websocket.Upgrader{}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println("Error with the upgrade. ;)")
+		return
+	}
+	for {
+	    messageType, p, err := conn.ReadMessage();
+	    if err != nil {
+		log.Println("Error with reading a message.")
+		return
+	    }
+	    if err := conn.WriteMessage(messageType, p); err != nil {
+		log.Println("Error with writing a message.")
+		return
+	    }
+	}
 }
 
 func main() {
-	listener, err := net.Listen("tcp", ":1234")
-	if err != nil {
-		fmt.Println("Your listener has an error")
-	} else {
-		for {
-			conn, err := listener.Accept()
-			if err != nil {
-				fmt.Println("The connection has an error")
-			} else {
-				go handleConnection(conn)
-			}
-		}
-	}
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":1234", nil))
 }
