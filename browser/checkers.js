0e2fdce0a84bb13844452bcb4ea1caaaf0a0a844
BODOMO <bodomo9999@gmail.com>
2020-04-08 17:00:24 -0700

    Institute Broken WebSocket

--- browser/checkers.js
+++ browser/checkers.js
@@ -1,21 +1,21 @@
-var windowWidth, windowHeight, canvas, ctx, dpiratio;
-var assets=[];
+var windowWidth, windowHeight, canvas, ctx, dpiratio, ws, gameStarted, assets=[];
+var enemyCheckers=[], friendlyCheckers=[];
 
 function onLoad() {
-	resize()
-	loadAssets()
-	canvas = document.getElementById("canvas")
-	dpiratio = window.devicePixelRatio
-	ctx = canvas.getContext("2d")
-	let x = windowWidth/2 - document.getElementById("middleblock").style.width/2
-	document.getElementById("middleblock").style.left = x + "px"
+	resize();
+	loadAssets();
+	canvas = document.getElementById("canvas");
+	dpiratio = window.devicePixelRatio;
+	ctx = canvas.getContext("2d");
+	ws = new WebSocket("ws://192.168.1.85:1234");
+	gameStarted = false;
 	
-	gameLoop()
+	gameLoop();
 }
 
 function loadAssets() {
-	assets[0] = new Image()
-	assets[0].src = "wallpaper.png"
+	assets[0] = new Image();
+	assets[0].src = "wallpaper.png";
 }
 
 function resize() {
@@ -23,22 +23,48 @@ function resize() {
 	windowHeight = window.innerHeight;
 	document.getElementById("canvas").style.width = windowWidth + "px";
 	document.getElementById("canvas").style.height = windowHeight + "px";
+	let adjustedWidth = windowWidth/2 - parseInt(document.getElementById("middleblock").style.width)/2;
+	let adjustedHeight = windowHeight/2 - parseInt(document.getElementById("middleblock").style.height)/2;
+	document.getElementById("middleblock").style.left = adjustedWidth + "px";
+	document.getElementById("middleblock").style.top = adjustedHeight + "px";
 }
 
 function gameLoop() {
-	update()
-	fix_dpi()
-	draw()
-	window.requestAnimationFrame(gameLoop)
+	update();
+	fix_dpi();
+	draw();
+	window.requestAnimationFrame(gameLoop);
 }
 
 function draw() {
-	ctx.filter = "blur(15px)"
-	ctx.drawImage(assets[0],0,0,canvas.width,canvas.height)
+	if (gameStarted) {
+		console.log("BBBBBB");
+	} else {
+		ctx.filter = "blur(15px)";
+		ctx.drawImage(assets[0],0,0,canvas.width,canvas.height);
+		console.log("AAAAA");
+	}
 }
 
 function update() {
 	
+}
+
+ws.onopen = function (event) {
+	console.log("Connection established");
+	ws.send("Heloooooooooooooooooo Comrade");
+}
+ws.onclose = function() {
+	console.log("Connection closed");
+}
+ws.onerror = function(error) {
+	console.log("Error: ", error);
+}
+
+function beginPlay() {
+	console.log("connection");
+	document.getElementById("middleblock").style.visibility = "hidden";
+	gameStarted = true;
 }
 
 function fix_dpi() {
