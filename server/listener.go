44d612a146f36082e533f265e652e6614d22ab45
Nathan Prisbrey <ntpbanana@gmail.com>
2020-04-09 22:53:50 +0000

    Finished Practicing with WebSockets in Golang

--- server/listener.go
+++ server/listener.go
@@ -5,49 +5,29 @@ import (
 	"net/http"
 	"github.com/gorilla/websocket"
 	"log"
-//	"time"
-//	"bufio"
 )
-
-//func handleConnection(conn net.Conn) {
-//	if _, err := conn.Write([]byte("Hey, what's up?")); err != nil {
-//		fmt.Println("Writing on the connection seems to have had an error: %v", err)
-//		return
-//	}
-//	conn.Close()
-//	bufReader := bufio.NewReader(conn)
-//	for {
-//		conn.SetReadDeadline(time.Now().Add(10 * time.Second))
-//		response, err := bufReader.ReadBytes('\n')
-//		if err != nil {fmt.Println("The noob connection didn't talk a lot so we dropped it.... :/");return}
-//		if _, err := conn.Write(append([]byte("I think that the following sucks: ")[:],response[:]...)); err != nil {
-//			fmt.Println("Writing on the connection seems to have had an error: %v", err)
-//			return
-//		}
-//	}
-//	conn.Write([]byte("You just got shut down. :)"))
-//	fmt.Println("One connection just closed itself")
-//	conn.Close()
-//}
 
 var upgrader = websocket.Upgrader{}
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	upgrader.CheckOrigin = func(r *http.Request) bool {return true}
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		fmt.Println("Error with the upgrade. ;)")
+		fmt.Println(err)
 		return
 	}
 	for {
-	    messageType, p, err := conn.ReadMessage();
-	    if err != nil {
-		log.Println("Error with reading a message.")
-		return
-	    }
-	    if err := conn.WriteMessage(messageType, p); err != nil {
-		log.Println("Error with writing a message.")
-		return
-	    }
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		fmt.Printf("Just recieved this message: %v\n", string(p))
+		if err := conn.WriteMessage(messageType, append([]byte("You just sent this, right: "), p[:]...)); err != nil {
+			log.Println(err)
+			return
+		}
 	}
 }
 
